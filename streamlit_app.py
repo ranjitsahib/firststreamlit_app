019041521a3f709e8b8b1bf9a1f949b4e74eb10b
ranjitsahib <77071828+ranjitsahib@users.noreply.github.com>
2023-01-26 12:37:45 +0000

    Update streamlit_app.py
    pick fruits by name

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
