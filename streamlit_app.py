d17d0d68b17f81a546fd58993ede3e1cd4ff715d
ranjitsahib <77071828+ranjitsahib@users.noreply.github.com>
2023-01-26 12:58:52 +0000

    Update streamlit_app.py
    added kabootar

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.header('Breakfast Menu')
 streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg, Kabootar')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
